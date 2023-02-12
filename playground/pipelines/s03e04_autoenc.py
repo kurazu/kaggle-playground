d8e62cbe2a9de831e83fb3658410c0b85bd32d3b
Tomasz Maćkowiak <kurazu@kurazu.net>
2023-02-12 18:31:59 +0100

    s03e04 with autoencoder

--- playground/pipelines/s03e04_autoenc.py
+++ playground/pipelines/s03e04_autoenc.py
@@ -1,4 +1,5 @@
 import logging
+import multiprocessing as mp
 from pathlib import Path
 from typing import ClassVar
 
@@ -7,6 +8,7 @@ import numpy as np
 import polars as pl
 import tensorflow as tf
 from matplotlib import pyplot as plt
+from returns.curry import partial
 from sklearn import metrics
 
 from ..feature_engineering import Features
@@ -16,6 +18,7 @@ from ..models.datasets import get_datasets
 from ..models.evaluation import get_ground_truth
 from ..models.predict import predict
 from . import ModelCustomizationInterface
+from .s03e04 import get_count_series
 
 logger = logging.getLogger(__name__)
 
@@ -27,6 +30,32 @@ class S03E04AutoencoderModelCustomization:
 
     @classmethod
     def feature_engineering(cls, raw_df: pl.LazyFrame) -> pl.LazyFrame:
+        initial_date = pl.datetime(2023, 1, 1)
+        materialized_timestamps = raw_df.select(
+            (
+                (pl.col("Time") * 1000).cast(pl.Duration(time_unit="ms")) + initial_date
+            ).alias("ts")
+        ).collect()["ts"]
+        logger.debug("Starting timestamps aggregation")
+        with mp.Pool() as pool:
+            count_series = list(
+                pool.imap_unordered(
+                    partial(get_count_series, materialized_timestamps),
+                    [
+                        100,  # a tenth of a second
+                        500,  # half a second
+                        1000,  # a second
+                        10000,  # ten seconds
+                        30000,  # thirty seconds
+                        60000,  # a minute
+                        300000,  # five minutes
+                        600000,  # ten minutes
+                        1800000,  # thirty minutes
+                        3600000,  # an hour
+                    ],
+                )
+            )
+        logger.debug("Finished timestamps aggregation")
         id_features: list[pl.Series | pl.Expr] = [pl.col(cls.id_column_name)]
         pca_features: list[pl.Series | pl.Expr] = [
             pl.col(f"V{i}").alias(f"pca_{i}") for i in range(1, 28 + 1)
@@ -42,7 +71,7 @@ class S03E04AutoencoderModelCustomization:
         )
 
         return raw_df.select(
-            id_features + pca_features + other_features + target_features
+            id_features + pca_features + other_features + count_series + target_features
         )
 
     @classmethod
@@ -63,7 +92,10 @@ class S03E04AutoencoderModelCustomization:
                 col for col in engineered_df.columns if col.startswith("pca_")
             },
             categorical_features=set(),
-            numerical_features={"amount"},
+            numerical_features={
+                col for col in engineered_df.columns if col.startswith("count_")
+            }
+            | {"amount"},
             cyclical_features={
                 "time": 60 * 60 * 24,
             },
@@ -82,14 +114,14 @@ class S03E04AutoencoderModelCustomization:
             shape=(no_features,), name="representation", dtype=tf.float32
         )
 
-        latent_space_size = 8
+        latent_space_size = 16
         dropout_rate = 0.5
 
         encoder = tf.keras.Sequential(
             [
-                tf.keras.layers.Dense(32, activation="relu"),
+                tf.keras.layers.Dense(64, activation="relu"),
                 tf.keras.layers.Dropout(dropout_rate),
-                tf.keras.layers.Dense(16, activation="relu"),
+                tf.keras.layers.Dense(32, activation="relu"),
                 tf.keras.layers.Dropout(dropout_rate),
                 tf.keras.layers.Dense(latent_space_size, activation="relu"),
             ],
@@ -97,9 +129,9 @@ class S03E04AutoencoderModelCustomization:
         )
         decoder = tf.keras.Sequential(
             [
-                tf.keras.layers.Dense(16, activation="relu"),
-                tf.keras.layers.Dropout(dropout_rate),
                 tf.keras.layers.Dense(32, activation="relu"),
+                tf.keras.layers.Dropout(dropout_rate),
+                tf.keras.layers.Dense(64, activation="relu"),
                 tf.keras.layers.Dropout(dropout_rate),
                 tf.keras.layers.Dense(no_features, activation="linear"),
             ],
